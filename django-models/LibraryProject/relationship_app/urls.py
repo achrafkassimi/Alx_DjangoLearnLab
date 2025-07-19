c80521de47b89b12a195eb1e7888c4c29752955c
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-07-19 15:27:33 +0100

    LibraryProject/relationship_app/urls.py doesn't contain: ["LogoutView.as_view(template_name=", "LoginView.as_view(template_name="]

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
     # User authentication URLs
-    path('login/', views.login_view, name='login'),
+    # path('login/', views.login_view, name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register_view, name='register'),
 ]
