ff8de4a58734ac11fdeb638d8f927916d0d91d24
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-07-25 22:06:58 +0100

    4- LibraryProject/bookshelf/admin.py doesn't contain: ["admin.site.register(CustomUser, CustomUserAdmin)"]

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -6,3 +6,24 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Columns shown in list view
     list_filter = ('author', 'publication_year')  # Sidebar filters
     search_fields = ('title', 'author')  # Search bar
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo',)
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo',)
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
