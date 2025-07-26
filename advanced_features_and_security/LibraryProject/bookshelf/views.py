09d4b432dcae942ff79d594d00de980cc2be185d
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-07-26 15:09:17 +0100

    LibraryProject/bookshelf/views.py doesn't contain: ["book_list", "raise_exception", "books"]

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect
+from relationship_app.models import Book
+
+@permission_required('relationship_app.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
+
+@permission_required('relationship_app.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        # save logic here
+        ...
+    return render(request, 'book_form.html')
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def book_edit(request, book_id):
+    # logic to edit book
+    ...
+
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def book_delete(request, book_id):
+    # logic to delete book
+    ...
