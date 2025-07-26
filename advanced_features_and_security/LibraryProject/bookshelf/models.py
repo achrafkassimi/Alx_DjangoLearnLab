a7f82a01d503765c94bc5fcab5b8706045e4a42e
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-07-26 15:05:52 +0100

    LibraryProject/bookshelf/models.py doesn't contain: ["can_create", "can_delete"]

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,12 +3,28 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    publication_year = models.IntegerField(default=2000)
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.title
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     publication_year = models.IntegerField()
+
+#     def __str__(self):
+#         return f"{self.title} by {self.author} ({self.publication_year})"
 
 
 # from django.contrib.auth.models import AbstractUser, BaseUserManager
