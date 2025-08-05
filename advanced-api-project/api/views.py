b67e38c16143d6c24845b7bc2d3cc90c85890a8e
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-05 23:27:11 +0100

    Checks for the implementation of the new views
    api/views.py doesn't contain: ["ListView", "UpdateView", "DeleteView"]

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -41,3 +41,35 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method in ['PUT', 'PATCH', 'DELETE']:
             return [permissions.IsAuthenticated()]
         return [permissions.AllowAny()]
+
+
+
+# ListView: List all books
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# DetailView: Get one book
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# CreateView: Add a new book
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# UpdateView: Modify a book
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# DeleteView: Delete a book
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
