6fadd50ea2cca881c3af271de741db29d6ff5fab
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-05 23:24:43 +0100

    1. Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/README.md
+++ advanced-api-project/README.md
@@ -3,3 +3,17 @@ Username (leave blank to use 'achraf'): achraf
 Email address: achraf@gmail.com
 Password: 1234
 Password (again): 1234
+
+
+## API Endpoints
+
+### Books
+- `GET /api/books/` — List all books
+- `POST /api/books/` — Create a new book (authenticated)
+- `GET /api/books/<id>/` — Retrieve a specific book
+- `PUT /api/books/<id>/` — Update a book (authenticated)
+- `DELETE /api/books/<id>/` — Delete a book (authenticated)
+
+### Permissions
+- Unauthenticated users: Can only read data.
+- Authenticated users: Can create, update, and delete books.
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import filters
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+# List all books or create a new one
+class BookListCreateView(generics.ListCreateAPIView):
+    """
+    GET: List all books (public)
+    POST: Create a new book (authenticated users only)
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'publication_year']
+
+    permission_classes = [IsAdminOrReadOnly]
+
+
+    # Allow only authenticated users to POST
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+    
+    def perform_create(self, serializer):
+        # Example: force assign a default author (e.g., ID = 1)
+        default_author = self.request.user.author_profile  # If you connect User to Author
+        serializer.save(author=default_author)
+
+# Retrieve, update, or delete a book
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # Restrict PUT, PATCH, DELETE to authenticated users
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
