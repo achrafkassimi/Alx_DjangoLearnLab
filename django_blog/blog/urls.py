ce9dd9b3c86f5517388e2c46504e8efaa3bf4c47
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-16 16:05:02 +0100

    Checks for “URL Configuration for New Features” task
    blog/urls.py doesn't contain: ["tags/<slug:tag_slug>/", "PostByTagListView.as_view()"]

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     # existing post CRUD routes...
     path("search/", views.search_posts, name="search-posts"),
     path("tags/<str:tag_name>/", views.posts_by_tag, name="posts-by-tag"),
+    path("tags/<slug:tag_slug>/", views.PostByTagListView.as_view(), name="posts_by_tag"),
+
 
     
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -189,3 +189,16 @@ def posts_by_tag(request, tag_name):
     tag = get_object_or_404(Tag, name=tag_name)
     posts = tag.posts.all()
     return render(request, "blog/posts_by_tag.html", {"tag": tag, "posts": posts})
+
+
+# blog/views.py
+from django.views.generic import ListView
+from .models import Post
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs["tag_slug"])
