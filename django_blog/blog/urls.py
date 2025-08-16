f1ca4df1fe34dd06495d197a20c0b6cb9925c397
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-16 15:07:48 +0100

    2. Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,13 +1,19 @@
-# blog/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # 👈 Add this
+    path('', views.home, name='home'),  # 👈 Home page
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logged_out.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+
+    # Blog Post CRUD URLs
+    path('posts/', views.PostListView.as_view(), name='post_list'),  # List all posts
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),  # View single post
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),  # Create new post
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),  # Edit post
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),  # Delete post
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,6 +6,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 # blog/views.py (Add this to your profile view)
 from .models import UserProfile
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm
 
 # User Registration View
 def register(request):
@@ -46,3 +52,54 @@ from django.shortcuts import render
 def home(request):
     # This will render the home.html template when you visit the root URL.
     return render(request, 'blog/home.html')
+
+
+# List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+# Detail view for a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+# Create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Automatically assign the logged-in user as the author
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+# Update an existing post
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # Ensure that only the author can update the post
+        if form.instance.author != self.request.user:
+            return redirect('post_list')  # Redirect if the user is not the author
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+# Delete an existing post
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+
+    def get_queryset(self):
+        # Ensure that only the author can delete the post
+        return Post.objects.filter(author=self.request.user)
