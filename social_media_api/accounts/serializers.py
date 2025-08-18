477395fb357a66de1689b8f29809a1a45d7f098b
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-18 17:06:20 +0100

    accounts/serializers.py doesn't contain: ["get_user_model().objects.create_user"]

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,20 +3,20 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token  # Import Token model for token generation
 from rest_framework.exceptions import AuthenticationFailed
 
-User = get_user_model()
+# User = get_user_model()
 
 # Custom User Registration Serializer
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']  # Customize this as per your model
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         password = validated_data.pop('password')
-        user = User.objects.create(**validated_data)
+        user = get_user_model().objects.create_user(**validated_data)
         user.set_password(password)  # Hash password
         user.save()
         Token.objects.create(user=user)  # Create a token for the user
@@ -31,7 +31,7 @@ class UserLoginSerializer(serializers.Serializer):
         username = data.get('username')
         password = data.get('password')
 
-        user = User.objects.filter(username=username).first()
+        user = get_user_model().objects.filter(username=username).first()
         if user is None:
             raise AuthenticationFailed('User not found')
 
