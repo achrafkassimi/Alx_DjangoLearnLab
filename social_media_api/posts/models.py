a2ba6a491a2d6343169e554dadca676bc6c2e972
achrafkassimi <achraf.kassimi.1995@gmail.com>
2025-08-18 17:45:46 +0100

    posts/views.py doesn't contain: ["Post.objects.filter(author__in=following_users).order_by"]

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 from django.conf import settings
 
-# Post model definition
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
-    title = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # assuming 'user' is the field
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.title
 
 # Comment model definition
 class Comment(models.Model):
